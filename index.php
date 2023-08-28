7493fa0ae47edb4fde38bbf96bdabaa8743033af
ThomF <seniorswanky@icloud.com>
2023-08-28 15:04:41 -0600

    learning WP hooks for posts

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-$names = array('thomas', 'brad', 'sean');
-$count = 0;
 
+while(have_posts()){
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
 
-while($count < count($names)) {
-    echo "<li>hello, my name is $names[$count]</li>";
-    $count++;
-}
+<?php }
 
+?>
